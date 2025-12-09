ae18e1a9e225d83819cf7e9c7f58a4c9aa1afc00
Romain Andrieux <roandrie@c1r3p1.42lehavre.fr>
2025-12-09 16:07:46 +0100

    Update from main repo

--- Python Module 00/ex7/ft_seed_inventory.py
+++ Python Module 00/ex7/ft_seed_inventory.py
@@ -4,6 +4,7 @@ def ft_seed_inventory(seed_types: str, quantity: int, unit: str):
     elif ("grams" in unit):
         print(f"{seed_types.capitalize()} seeds: {quantity} {unit} total")
     elif ("area" in unit):
-        print(f"{seed_types.capitalize()} seeds: {quantity} {unit} covers X square meters")
+        print(f"{seed_types.capitalize()} seeds: {quantity} ")
+        print(f"{unit} covers X square meters")
     else:
         print("Unknown unit type")
