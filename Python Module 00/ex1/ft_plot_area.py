f679a99ba1d02def6a9a9f8ba7371b1c75e803cc
Overtek <90642113+Overtekk@users.noreply.github.com>
2025-12-10 12:18:35 +0100

    Update ft_plot_area.py

--- Python Module 00/ex1/ft_plot_area.py
+++ Python Module 00/ex1/ft_plot_area.py
@@ -2,6 +2,6 @@ def ft_plot_area():
     length = int(input("Enter length: "))
     width = int(input("Enter width: "))
 
-    area = length + width
+    area = length * width
 
     print(f"Plot Area: {area}")
